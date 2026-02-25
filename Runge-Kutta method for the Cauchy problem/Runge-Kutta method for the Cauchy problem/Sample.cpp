f469630c9ed2ee8b960470e92410bff777b500e4
Korobokkk <bbb754748@gmail.com>
2026-02-25 23:51:11 +0300

    first v. RungeKutta method. WARRING: contains vector out-of-bounds and other errors. Needs to be improved. Added GlobalErrorRate check, micro fix in CreateSampleData()

--- Runge-Kutta method for the Cauchy problem/Runge-Kutta method for the Cauchy problem/Sample.cpp
+++ Runge-Kutta method for the Cauchy problem/Runge-Kutta method for the Cauchy problem/Sample.cpp
@@ -2,17 +2,50 @@
 #include "Sample.h"
 
 #include <iostream>
+#include <vector>
 
-void Sample::CreateSampleData(int var, double x_0, double x_end, double h, double u_0) {
-	this->variant_num = 1;
-	this->x_0 = 0;
-	this->x_end = 10;
-	this->h = 1;
-	this->u_0 = 2;
+void Sample::CreateSampleData(int var, double x_0, double x_end, double h, double u_0, double epsilon) {
+	this->variant_num = var;
+	this->x_0 = x_0;
+	this->x_end = x_end;
+	this->h = h;
+	this->u_0 = u_0;
+	this->epsilon = epsilon;
 	return;
 };
 void Sample::PrintSampleData() {
 	std::cout << "var: "<< variant_num <<"\n x_0: " << x_0 << "\n x_end: " << x_end << "\n h: "  << h << "\n u_0: " << u_0;
-	
 }
 
+void Sample::GlobalErrorRate() {
+
+}
+void Sample::LocalErrorRate() {
+
+}
+
+void Sample::MethodRungeKutta() {
+	// dy / dx= (-1)**var_num * var_num / 2 * u 
+	// WARING: sample not using x 
+	double lyambda = pow(-1, variant_num) * variant_num / 2;
+	std::vector<double> u(4);
+	std::vector<double> k(4);
+	u[0] = u_0;
+
+	for (int i = 0; i < 3; i++) { //u_0 skipped
+		double h_n = h;
+		//WARING: sample not using x 
+		k[0] = lyambda * u[i];
+		k[1] = lyambda * (u[i] + h_n / 2 * k[0]);
+		k[2] = lyambda * (u[i] + h_n / 2 * k[1]);
+		k[3] = lyambda * (u[i] + h_n * k[2]);
+
+		//WARING: sample not using x 
+		u[i+1] = u[i] + h_n / 6 * (k[0] + 2 * k[1] + 2 * k[2] + k[3]);
+
+		//GlobalErrorRate
+		std::vector<double> global_error_rate(4, 0.0);
+		global_error_rate[i+1] = u[i + 1] - u_0 + h * (i + 1);
+	}
+	
+}
--- Runge-Kutta method for the Cauchy problem/Runge-Kutta method for the Cauchy problem/Sample.h
+++ Runge-Kutta method for the Cauchy problem/Runge-Kutta method for the Cauchy problem/Sample.h
@@ -4,24 +4,24 @@
 ref class Sample
 {
 public:
-	Sample() {
-		CreateSampleData(2, 1.0, 10.0, 0.0, 1.0);
-	}
 	//тест данные
-	Sample(int var, double x_0, double x_end, double h, double u_0) {
-		CreateSampleData(var, x_0, x_end, h, u_0);
+	Sample() {
+		CreateSampleData(2, 1.0, 10.0, 0.0, 1.0, 1.0);
 	}
-	void CreateSampleData(int var, double x_0, double x_end, double h, double u_0);
+	
+	Sample(int var, double x_0, double x_end, double h, double u_0, double epsilon) {
+		CreateSampleData(var, x_0, x_end, h, u_0, epsilon);
+	}
+	void CreateSampleData(int var, double x_0, double x_end, double h, double u_0, double epsilon);
 	void PrintSampleData();
+	void MethodRungeKutta();
+	void GlobalErrorRate();
+	void LocalErrorRate();
 private:
 	int variant_num;
 	double x_0;
 	double x_end;
 	double h;
 	double u_0;
+	double epsilon;
 };
-
-//int main() {
-//	Sample tmp;
-//	tmp.PrintSampleData();
-//}
