3d5f3265b057194d0aacad65e3f531bd7da6757c
Korobokkk <bbb754748@gmail.com>
2026-02-28 20:15:53 +0300

    new realisation

--- Runge-Kutta method for the Cauchy problem/Runge-Kutta method for the Cauchy problem/Sample.cpp
+++ Runge-Kutta method for the Cauchy problem/Runge-Kutta method for the Cauchy problem/Sample.cpp
@@ -52,69 +52,78 @@ void Sample::MethodRungeKutta() {
                    counter_div, counter_mul,
                    u_true, global_error };
 
-	*/
+	*/ //for save
+	
+	StepResult row;
 
-	std::vector<double> u = {this->u_0};
-	std::vector<double> x = {this->x_0};
-	std::vector<double> global_error_rate(1, 0.0);
-	std::vector<double> local_error_rate(1, 0.0);
+	double u_curr = u_0;
+	double x_curr = x_0;
 	double lyambda = pow(-1, variant_num) * variant_num / 2;
 	double k1, k2, k3, k4;
 	double curr_h = h;
+	double next_h = h;
 
-	//double x_curr = x_0;
+	double div_counter = 0;
+	double mul_counter = 0;
 	int iter_counter = 0;
-	while (x[iter_counter] < x_end) {
+	while (x_curr < x_end) {
+		curr_h = next_h;
 		if (curr_h < 1e-12) {
 			std::cout << "Step too small\n";
 			break;
 		}
 		//WARING: sample not using x 
-		k1 = lyambda * u[iter_counter];
-		k2 = lyambda * (u[iter_counter] + curr_h / 2 * k1);
-		k3 = lyambda * (u[iter_counter] + curr_h / 2 * k2);
-		k4 = lyambda * (u[iter_counter] + curr_h * k3);
+		k1 = lyambda * u_curr;
+		k2 = lyambda * (u_curr + curr_h / 2 * k1);
+		k3 = lyambda * (u_curr + curr_h / 2 * k2);
+		k4 = lyambda * (u_curr + curr_h * k3);		
 
-		u.push_back(u.back() + curr_h / 6 * (k1 + 2 * k2 + 2 * k3 + k4));
-		x.push_back(x.back() + curr_h);
+		double u_next = u_curr + curr_h / 6 * (k1 + 2 * k2 + 2 * k3 + k4);
+		double x_next = x_curr + curr_h;
 
-		global_error_rate.push_back(u[iter_counter + 1] -
-									u_0 * exp(lyambda * (x[iter_counter+1] - x_0)));
+		double next_global_error = u_next - u_0 * exp(lyambda * (x_next - x_0));
 
-		double f1 = count_next_u(u[iter_counter], curr_h / 2, lyambda);
+		double f1 = count_next_u(u_curr, curr_h / 2, lyambda);
 		double f2 = count_next_u(f1, curr_h / 2, lyambda);
-		local_error_rate.push_back(f2 - u[iter_counter + 1]);
-		double S = std::fabs(local_error_rate.back() / 15.0);
 
+		double next_local_error(std::fabs(f2 - u_next));
+		
+		double S = std::fabs(next_local_error / 15.0);
+		
+		bool IsPositive = true;
 		if (IsDinamicStep) {
 			if (epsilon / 32.0 <= S && S <= epsilon) {
+				next_h = curr_h;
 				std::cout << "\n\ngood, make print";
 			}
 			else if (S < epsilon / 32.0) {
-				curr_h *= 2;
+				next_h *= 2;
+				mul_counter += 1;
 				std::cout << "\n\nnext h*=2 cool, make print";
 			}
 			else {
-				std::cout << "very bad. Restart step";
-				curr_h /= 2.0;
-				u.pop_back();
-				x.pop_back();
-				global_error_rate.pop_back();
-				local_error_rate.pop_back();
+				std::cout << "\nvery bad.Restart step\n";
+				next_h /= 2.0;
+				div_counter += 1;
+				IsPositive = false;
 				continue;
 			}
 		}
+		if (IsPositive) {
 
+			u_curr = u_next;
+			x_curr = x_next;
+		}
 		//checks out_of ranges
-		curr_h = std::min(x_end - x.back(), curr_h);
+		next_h = std::min(x_end - x_next, next_h);
 
 		std::cout << "\niter: " << iter_counter <<
-			"\n h_iter: " << curr_h <<
+			"\n h_iter: " << next_h <<
 			"\n k: " << k1 << " " << k2 << " " << k3 << " " << k4 <<
-			"\n u_next: " << u[iter_counter + 1] <<
-			"\n x_next: " << x[iter_counter + 1] <<
-			"\n global_error: " << global_error_rate[iter_counter + 1] <<
-			"\n local_error: " << local_error_rate[iter_counter + 1];
+			"\n u_next: " << u_next <<
+			"\n x_next: " << x_next <<
+			"\n global_error: " << next_global_error <<
+			"\n local_error: " << next_local_error;
 		
 		iter_counter++;
 	}
